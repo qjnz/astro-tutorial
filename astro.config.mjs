c9c45b3cf28df001a9450ab21880099e900bfb78
Qiujie Li <liqiujie2008@gmail.com>
2024-09-07 22:16:15 +1200

    Tutorial 6.1: Build first Astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fancy-blog.netlify.app/'
+  site: 'https://fancy-blog.netlify.app/',
+  integrations: [preact()]
 });
